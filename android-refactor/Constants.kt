4e85acd55ef116bccdae848327f2ec952ae6d5ce
Yellow0124 <412631250@o365.tku.edu.tw>
2026-06-01 00:22:02 +0800

    feat: Add Constants.kt - Centralized constants and color definitions

---
+++ android-refactor/Constants.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+// 常數定義
+const val API_BASE_URL = "http://192.168.0.138:4000"
+
+// 顏色定義
+val Canvas = androidx.compose.ui.graphics.Color(0xFFF6F1E8)
+val SurfaceIvory = androidx.compose.ui.graphics.Color(0xFFFFFCF6)
+val StageBlack = androidx.compose.ui.graphics.Color(0xFF08111F)
+val Ink = androidx.compose.ui.graphics.Color(0xFF111827)
+val Muted = androidx.compose.ui.graphics.Color(0xFF6B7280)
+val Mist = androidx.compose.ui.graphics.Color(0xFFE9DFD1)
+val FineLine = androidx.compose.ui.graphics.Color(0xFFD8CBBE)
+val Porcelain = androidx.compose.ui.graphics.Color(0xFFFFFFFF)
+val Gold = androidx.compose.ui.graphics.Color(0xFFD8A84E)
+val GoldSoft = androidx.compose.ui.graphics.Color(0xFFFFF2CF)
+val Cobalt = androidx.compose.ui.graphics.Color(0xFF245CFF)
+val CobaltSoft = androidx.compose.ui.graphics.Color(0xFFE8EEFF)
+val Success = androidx.compose.ui.graphics.Color(0xFF0F8A5F)
+val Rose = androidx.compose.ui.graphics.Color(0xFFD64545)
+val Plum = androidx.compose.ui.graphics.Color(0xFF7C3AED)
